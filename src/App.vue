8f88d6d4abd4a049e2718cdcf698e048f7fdd885
Firdesbeyzanur <99497565+Firdesbeyzanur@users.noreply.github.com>
2023-01-26 16:21:14 +0300

    Files Added
    Files Added

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <note-comp @note-result="notesList"></note-comp>
+  <note-output :results="savedNotes"></note-output>
+</template>
+
+<script>
+import NoteComp from "./components/Notes/NoteComp.vue";
+import NoteOutput from "./components/Notes/NoteOutput.vue";
+export default {
+  components: {
+    NoteComp,
+    NoteOutput,
+  },
+  data(){
+    return {
+      savedNotes: [],
+    }
+  },
+  methods: {
+    notesList(listData){
+      const listOutput = {
+        note: listData.note,
+        onemlilikDerecesi: listData.onemlilikDerecesi,
+        id: new Date().toISOString(),
+      }
+      this.savedNotes.push(listOutput);
+      console.log(listOutput);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Notes/NoteOutput.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <base-card>
+      <h1>Notes</h1>
+      <div>
+        <base-button>Notları Görüntüle</base-button>
+      </div>
+      <ul>
+        <note-result
+          v-for="result in results"
+          :key="result.id"
+          :note="result.note"
+          :onemlilikDerecesi="result.onemlilikDerecesi"
+        >
+        </note-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import NoteResult from "./NoteResult.vue";
+export default {
+   props:['results'],
+  components: {
+    NoteResult,
+  },
+};
+</script>
+
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+h1 {
+  color: rgb(25, 25, 112);
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 40px;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
